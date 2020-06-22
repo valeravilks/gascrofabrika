647143f01fa4889282ca8c549721456af5d45396
valeravilks <valeravilks@gmail.com>
2020-06-22 11:47:13 +0300

    New day

--- page-gallery.php
+++ page-gallery.php
@@ -11,33 +11,6 @@ get_header();
         <h1 class="h1">
             <?php the_title() ?>
         </h1>
-<!--        --><?php
-//        $images = get_field('gallery-photo'); ?>
-<!--        <div class="grid are-images-unloaded">-->
-<!--            <div class="grid__col-sizer"></div>-->
-<!--            <div class="grid__gutter-sizer"></div>-->
-<!---->
-<!--         --><?php //if( $images ): ?>
-<!---->
-<!--            --><?php //foreach( $images as $image ): ?>
-<!--                 <div class="grid__item">-->
-<!--                     <img src="--><?php //echo esc_url($image['sizes']['large']); ?><!--" alt="">-->
-<!--                 </div>-->
-<!--            --><?php //endforeach; ?>
-<!---->
-<!--        --><?php //endif; ?>
-<!--        </div>-->
-<!---->
-<!--        <div class="page-load-status">-->
-<!--            <div class="loader-ellips infinite-scroll-request">-->
-<!--                <span class="loader-ellips__dot"></span>-->
-<!--                <span class="loader-ellips__dot"></span>-->
-<!--                <span class="loader-ellips__dot"></span>-->
-<!--                <span class="loader-ellips__dot"></span>-->
-<!--            </div>-->
-<!--            <p class="infinite-scroll-last">End of content</p>-->
-<!--            <p class="infinite-scroll-error">No more pages to load</p>-->
-<!--        </div>-->
         <?php echo do_shortcode('[cc_img_gallery id="131"]')?>
     </div>
 </main>
---
+++ page-stocks.php
@@ -0,0 +1,55 @@
+<?php
+/*
+* template name: Акции
+*/
+
+
+get_header();
+?>
+
+<main class="stocks">
+    <div class="container">
+        <h1 class="title">
+            <?php the_title() ?>
+        </h1>
+        <?php
+
+        $args = array(
+            'posts_per_page' => 10,
+            'post_type' => 'stocks'
+        );
+        $query = new WP_Query( $args );
+        if( $query->have_posts() ){ ?>
+            <?php
+            while( $query->have_posts() ){
+                $query->the_post();
+                ?>
+                <div class="block">
+                    <div
+                        class="img"
+                        style="background: url(<?php the_field('stocks-img')?>)"
+                    ></div>
+                    <div class="content">
+                        <h2>
+                            <?php the_title()?>
+                        </h2>
+                        <div class="text">
+                            <?php the_field('stocks-text')?>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+
+                <?php
+            } ?>
+            <?php
+
+            wp_reset_postdata(); // сбрасываем переменную $post
+        } ?>
+
+    </div>
+</main>
+
+<?php
+get_footer();
+?>
